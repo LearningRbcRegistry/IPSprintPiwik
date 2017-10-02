1ff034b36e045c35cf27f0b250957c6b6e5c474c
timzok <kader@timzorane.com>
2017-10-02 23:54:38 +0200

    css

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -30,16 +30,22 @@ export class HeroDetailComponent implements OnInit {
   }
 
   save(): void {
-    console.log("SAVE DETAIL HERO");
-    this.angulartics2Piwik.eventTrack.next({action: 'hero-detail',properties: {category: 'hero detail',label: 'save'}});
-    console.log("SAVE DETAIL HERO AFTER");
+      console.log('SAVE DETAIL HERO');
+      this.angulartics2Piwik.eventTrack.next({
+          action: 'hero-detail',
+          properties: {category: 'hero detail', label: 'save'}
+      });
+      console.log('SAVE DETAIL HERO AFTER');
     this.heroService.update(this.hero)
         .then(() => this.goBack());
   }
 
   goBack(): void {
 
-    this.angulartics2Piwik.eventTrack.next({action: 'hero-detail',properties: {category: 'hero detail',label: 'back'}});
+      this.angulartics2Piwik.eventTrack.next({
+          action: 'hero-detail',
+          properties: {category: 'hero detail', label: 'back'}
+      });
     this.location.back();
   }
 }
